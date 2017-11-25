27d5fee3f6a79302bab429821e36b93d23f82b11
Aria Pahlavan <apahlavan1@utexas.edu>
2017-11-25 14:58:18 -0800

    fix bug where VectorStream wasn't performing remove and filter correctly

--- Main.cpp
+++ Main.cpp
@@ -3,36 +3,109 @@
 //
 #include <iostream>
 #include "Stream.h"
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <experimental/optional>
+#include <chrono>
+#include <thread>
 
+using namespace std::experimental;
 using namespace std;
 
+void premetiveWay(const vector<int> &vec);
+void functionalWay(const vector<int> &vec);
+void functionalWay2(const vector<int> &vec);
+
+auto ellapsedTimeMs = [](clock_t start) { return "Time: " + to_string((std::clock() - start) / (double) (CLOCKS_PER_SEC / 1000)) + " ms"; };
+auto ellapsedTime = [](clock_t start) { return "Time: " + to_string((std::clock() - start) / (double) (CLOCKS_PER_SEC / 1000 * 1000)) + " s"; };
+
 int main() {
+    vector<int> vec;
 
-    vector<int> val;
-    srand (time(NULL));
+    srand(time(nullptr));
 
-    for (int i = 0; i < 100000; ++i) {
-        i = rand() % 500 + 1;
-        val.emplace_back(i);
+//  1000000000
+    for (int index = 0; index < 100000; index++) {
+        int random_integer = rand() % 10 + 0;
+        vec.push_back(random_integer);
+    }
+
+    cout << endl;
+    {
+        auto start = std::clock();
+        cout << "-----------------------------\n";
+        premetiveWay(vec);
+        std::cout << ellapsedTimeMs(start) << std::endl;
+        std::cout << ellapsedTime(start) << std::endl;
+        cout << "-----------------------------\n";
+
+    }
+
+    {
+        auto start = std::clock();
+        cout << "-----------------------------\n";
+        functionalWay(vec);
+        std::cout << ellapsedTimeMs(start) << std::endl;
+        std::cout << ellapsedTime(start) << std::endl;
+        cout << "-----------------------------\n";
+    }
+
+    {
+        auto start = std::clock();
+        cout << "-----------------------------\n";
+        functionalWay2(vec);
+        std::cout << ellapsedTimeMs(start) << std::endl;
+        std::cout << ellapsedTime(start) << std::endl;
+        cout << "-----------------------------\n";
     }
 
 
-    cout <<
-         Stream::make<int>(val)
-                 .remove([](int x) { return x == 2; })
-                 .filter([](int x){ return x<5; })
-                 .flatmap<string>([](int x) { return std::to_string(x); })
-                 .reduce([](string a, string b) { return a + " " + b; }, "{")
-                 .get()
-         <<
-            " }"
-         << endl;
-
-    cout << "------------" << endl;
-
-
     return 0;
+}
+
+void busyWork(){
+    unsigned int sleepTime = 5000;
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
+}
+
+void premetiveWay(const vector<int> &vec) {
+    cout << "Primitive" << endl;
+    auto sum = 0;
+
+    for (auto val : vec) {
+        if (val == 0) { continue; }             // remove zeros
+        if (val % 2 == 0) { continue; }         // keep odds
+        auto valStr = to_string(val);           // to string
+//        cout << valStr << " ";                  // print
+        auto valInt = stoi(valStr);             // to int
+        sum += valInt;                          // sum up
+    }
+    busyWork();
+    cout << endl << "result: " << sum << endl;  // get result
+}
+
+void functionalWay(const vector<int> &vec) {
+    cout << "Functional 1" << endl;
+    auto result = Stream::make<int>(vec)
+            .remove([](int x) { return x < 1; })                  // remove zeros
+            .filter([](int x) { return x % 2; })                  // keep odds
+            .map<string>([](int x) { return to_string(x); })  // to string
+//            .forEach([](string x) { cout << x << " "; })          // print
+            .map<int>([](string x) { return stoi(x); })        // to int
+            .reduce(plus<int>(), 0);                               // sum up
+
+    busyWork();
+    cout << endl << "result: " << result.get() << endl; // get result
+}
+
+void functionalWay2(const vector<int> &vec) {
+    cout << "Functional 2" << endl;
+    auto result = Stream::make2<int>(vec)
+            .remove([](int x) { return x < 1; })                  // remove zeros
+            .filter([](int x) { return x % 2; })                  // keep odds
+            .flatmap<string>([](int x) { return to_string(x); })  // to string
+//            .forEach([](string x) { cout << x << " "; })          // print
+            .flatmap<int>([](string x) { return stoi(x); })        // to int
+            .reduce(plus<int>(), 0);                               // sum up
+
+    busyWork();
+    cout << endl << "result: " << result << endl;
 }
--- Stream.h
+++ Stream.h
@@ -10,9 +10,14 @@
 #include <map>
 #include <future>
 #include "Stream.h"
+#include <numeric>
 
 template<typename T>
 class VectorStream;
+
+
+template<typename T>
+class VectorStream2;
 
 template<typename M, typename N>
 class MapStream;
@@ -24,6 +29,11 @@ public:
         std::promise<std::vector<T>> vec;
         vec.set_value(v);
         return VectorStream<T>(move(vec));
+    }
+
+    template <typename T>
+    static auto make2(std::vector<T> v) {
+        return VectorStream2<T>(v);
     }
 
     template <typename T, typename U>
@@ -40,13 +50,11 @@ private:
 
     futureVector fv;
 
-public:
-    explicit VectorStream(promiseVector v) : fv(v.get_future()) {
-    }
-    explicit VectorStream(futureVector v) : fv(std::move(v)) {}
+    template <typename fun>
+    auto launch(fun functor){ return std::async(std::launch::async, functor);}
 
     template<typename U, typename fun>
-    auto _flatmap(fun mapper) {
+    auto _map(fun mapper) {
         std::vector <U> newV;
         auto v = fv.get();
         newV.resize(v.size());
@@ -55,27 +63,24 @@ public:
         return newV;
     }
 
-    template<typename U, typename fun>
-    auto flatmap(fun mapper) {
-        auto fu = std::async(std::launch::async, [=](){ return _flatmap<U>(mapper); });
-        return VectorStream<U>(std::move(fu));
-    }
-
     template <typename fun>
     auto _remove(fun predicate){
-        std::vector <T> newV;
         auto v = fv.get();
-        newV.resize(v.size());
+        std::vector <T> newV(v);
 
         auto garbage = std::remove_copy_if(v.begin(), v.end(), newV.begin(), predicate);
-        newV.erase(garbage);
+        newV.erase(garbage, newV.end());
         return newV;
     }
 
     template <typename fun>
-    auto remove(fun predicate){
-        auto fu = std::async(std::launch::async, [=](){ return _remove(predicate); });
-        return VectorStream<T>(std::move(fu));
+    auto _filter(fun predicate){
+        auto v = fv.get();
+        std::vector <T> newV(v);
+
+        auto garbage = std::copy_if(v.begin(), v.end(), newV.begin(), predicate);
+        newV.erase(garbage, newV.end());
+        return newV;
     }
 
     template <typename fun>
@@ -85,37 +90,55 @@ public:
     }
 
     template <typename fun>
-    auto reduce(fun reducer, T init){
-        auto fu = std::async(std::launch::async, [=](){ return _reduce(reducer, init); });
-        return fu;
-    }
-
-    template <typename fun>
-    auto _filter(fun predicate){
-        std::vector <T> newV;
+    auto _forEach(fun action){
         auto v = fv.get();
-        newV.resize(v.size());
+        std::for_each(v.begin(), v.end(), action);
 
-        auto garbage = std::copy_if(v.begin(), v.end(), newV.begin(), predicate);
-        newV.erase(garbage);
-        return newV;
+        return v;
     }
 
-    template <typename fun>
-    auto filter(fun predicate){
-        auto fu = std::async(std::launch::async, [=](){ return _filter(predicate); });
+public:
+    explicit VectorStream(promiseVector v) : fv(v.get_future()) {
+    }
+    explicit VectorStream(futureVector v) : fv(std::move(v)) {}
+
+    template<typename U, typename fun>
+    auto map(fun mapper) {
+        auto fu = launch([=](){ return _map<U>(mapper); });
+        return VectorStream<U>(std::move(fu));
+    }
+
+    template<typename fun>
+    auto map(fun mapper) {
+        auto fu = launch([=](){ return _map<T>(mapper); });
         return VectorStream<T>(std::move(fu));
     }
 
     template <typename fun>
-    auto forEach(fun action){
-        auto v = fv.get();
-        std::for_each(v.begin(), v.end(), action);
-
-        promiseVector pv;
-        pv.set_value(v);
-        return VectorStream<T>(std::move(pv));
+    auto remove(fun predicate){
+        auto fu = launch([&](){ return this->_remove(predicate); });
+        return VectorStream<T>(std::move(fu));
     }
+
+    template <typename fun>
+    auto filter(fun predicate){
+        auto fu = launch([=](){ return _filter(predicate); });
+        return VectorStream<T>(std::move(fu));
+    }
+
+    template <typename fun>
+    auto reduce(fun reducer, T init){
+        auto fu = launch([=](){ return _reduce(reducer, init); });
+        return fu;
+    }
+
+    template <typename fun>
+    auto forEach(fun action){
+        auto fu = launch([=](){ return _forEach(action); });
+        return VectorStream<T>(std::move(fu));
+    }
+
+    auto get() { return fv.get(); }
 
     template <typename fun>
     auto collect(fun collector){
@@ -124,6 +147,60 @@ public:
 
     auto operator|(const VectorStream &rhs) const {
         return true;
+    }
+};
+
+///@deprecated: for performance testing purposes only.
+template<typename T>
+class [[deprecated("use \'VectorStream\' instead.")]] VectorStream2 : public Stream {
+private:
+    std::vector<T> v;
+
+public:
+    explicit VectorStream2(const std::vector<T> &v) : v(v) {}
+
+    template<typename U, typename fun>
+    auto flatmap(fun mapper) {
+        std::vector <U> newV;
+        newV.resize(v.size());
+
+        std::transform(v.begin(), v.end(), newV.begin(), mapper);
+        return VectorStream2<U>(newV);
+    }
+
+    template <typename fun>
+    auto remove(fun predicate){
+        std::vector <T> newV;
+        newV.resize(v.size());
+
+        auto garbage = std::remove_copy_if(v.begin(), v.end(), newV.begin(), predicate);
+        newV.erase(garbage, newV.end());
+
+        return VectorStream2<T>(newV);
+    }
+
+    template <typename fun>
+    auto filter(fun predicate){
+        std::vector <T> newV;
+        newV.resize(v.size());
+
+        auto garbage = std::copy_if(v.begin(), v.end(), newV.begin(), predicate);
+        newV.erase(garbage, newV.end());
+
+        return VectorStream2<T>(newV);
+    }
+
+
+    template <typename fun>
+    auto reduce(fun reducer, T init){
+        return std::accumulate(v.begin(), v.end(), init);
+    }
+
+    template <typename fun>
+    auto &forEach(fun action){
+        std::for_each(v.begin(), v.end(), action);
+
+        return *this;
     }
 };
 
